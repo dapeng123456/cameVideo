42ecd799df7aa95385e2124b7a8c990372de656a
dapeng123456 <619235706@qq.com>
2016-10-20 16:08:56 +0800

    提交

---
+++ SYPCameVideo/SiriViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SiriViewController.swift
+//  SYPCameVideo
+//
+//  Created by admin on 2016/10/20.
+//  Copyright © 2016年 SYP. All rights reserved.
+//
+
+import UIKit
+
+class SiriViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = VCBaseViewColor()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
