b086203c5185c0c307f87b2b5ed464336fe46d6f
dapeng <shiyapeng@yixia.com>
2017-03-09 14:18:46 +0800

    修改一下下

--- SYPCameVideo/VCViewControllers/Tabbar/VCTabBarController.swift
+++ SYPCameVideo/VCViewControllers/Tabbar/VCTabBarController.swift
@@ -12,27 +12,48 @@ class VCTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addChildViewControllers()
+//        UITabBar.appearance().backgroundColor = VCRGBColor(r: 18, g: 19, b: 20, a: 1)
+//        UITabBar.appearance().backgroundImage=UIImage()
+        let array = NSMutableArray()
+        let homeVC = VCHomeViewController()
+        let nav = VCNavigationController.init(rootViewController: homeVC)
+        array.add(nav)
+        
+        let foundVC = VCFoundController()
+        let foundNav = VCNavigationController.init(rootViewController: foundVC)
+        array.add(foundNav)
+        
+        let myVC = VCMyViewController()
+        let myNav = VCNavigationController.init(rootViewController: myVC)
+        array.add(myNav)
+        
+        self.viewControllers
+        
+        
         // Do any additional setup after loading the view.
     }
 
-    private func addChildViewControllers(){
-        addChildViewController(VCHomeViewController(), titile: "主页", imageName: "SYPHome")
-        addChildViewController(VCFoundController(), titile: "我", imageName: "SYPMy")
-        addChildViewController(VCMyViewController(), titile: "我", imageName: "SYPMy")
-    }
+//    private func addChildViewControllers(){
+//        addChildViewController(VCHomeViewController(), titile: "主页", imageName: "SYPHome")
+//        addChildViewController(VCFoundController(), titile: "我", imageName: "SYPMy")
+//        addChildViewController(VCMyViewController(), titile: "我", imageName: "SYPMy")
+//    }
     
-    private func addChildViewController(_ childController: UIViewController, titile:String, imageName:String) {
-        
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "Select")
-        
-        childController.tabBarItem.title = titile
-        
-        let nav = VCNavigationController()
-        nav.addChildViewController(childController)
-        addChildViewController(nav)
-    }
+//    private func addChildViewController(_ childController: UIViewController, titile:String, imageName:String) {
+//
+//        var image:UIImage = UIImage(named: imageName)!
+//        var selectedimage:UIImage = UIImage(named: imageName + "Select")!;
+//        image = image.withRenderingMode(UIImageRenderingMode.alwaysOriginal);
+//        selectedimage = selectedimage.withRenderingMode(UIImageRenderingMode.alwaysOriginal);
+//        
+//        childController.tabBarItem.image = image
+//        childController.tabBarItem.selectedImage = selectedimage
+//        childController.tabBarItem.title = titile
+//        
+//        let nav = VCNavigationController()
+//        nav.addChildViewController(childController)
+//        addChildViewController(nav)
+//    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
