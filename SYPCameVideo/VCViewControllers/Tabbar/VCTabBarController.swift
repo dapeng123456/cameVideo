cffc8ea274456bc64da0dc495787b3b08bc31cd7
dapeng <shiyapeng@yixia.com>
2017-04-05 09:16:08 +0800

    暂时提交

--- SYPCameVideo/VCViewControllers/Tabbar/VCTabBarController.swift
+++ SYPCameVideo/VCViewControllers/Tabbar/VCTabBarController.swift
@@ -12,48 +12,15 @@ class VCTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        UITabBar.appearance().backgroundColor = VCRGBColor(r: 18, g: 19, b: 20, a: 1)
-//        UITabBar.appearance().backgroundImage=UIImage()
-        let array = NSMutableArray()
-        let homeVC = VCHomeViewController()
-        let nav = VCNavigationController.init(rootViewController: homeVC)
-        array.add(nav)
+        UITabBar.appearance().backgroundColor = VCRGBColor(r: 18, g: 19, b: 20, a: 1)
+        UITabBar.appearance().backgroundImage=UIImage()
         
-        let foundVC = VCFoundController()
-        let foundNav = VCNavigationController.init(rootViewController: foundVC)
-        array.add(foundNav)
-        
-        let myVC = VCMyViewController()
-        let myNav = VCNavigationController.init(rootViewController: myVC)
-        array.add(myNav)
-        
-        self.viewControllers
-        
+        //addChildViewControllers()
         
         // Do any additional setup after loading the view.
     }
 
-//    private func addChildViewControllers(){
-//        addChildViewController(VCHomeViewController(), titile: "主页", imageName: "SYPHome")
-//        addChildViewController(VCFoundController(), titile: "我", imageName: "SYPMy")
-//        addChildViewController(VCMyViewController(), titile: "我", imageName: "SYPMy")
-//    }
-    
-//    private func addChildViewController(_ childController: UIViewController, titile:String, imageName:String) {
-//
-//        var image:UIImage = UIImage(named: imageName)!
-//        var selectedimage:UIImage = UIImage(named: imageName + "Select")!;
-//        image = image.withRenderingMode(UIImageRenderingMode.alwaysOriginal);
-//        selectedimage = selectedimage.withRenderingMode(UIImageRenderingMode.alwaysOriginal);
-//        
-//        childController.tabBarItem.image = image
-//        childController.tabBarItem.selectedImage = selectedimage
-//        childController.tabBarItem.title = titile
-//        
-//        let nav = VCNavigationController()
-//        nav.addChildViewController(childController)
-//        addChildViewController(nav)
-//    }
+        
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- SYPCameVideo/VCViewControllers/VCMineController/VCMyScanViewController.swift
+++ SYPCameVideo/VCViewControllers/VCMineController/VCMyScanViewController.swift
@@ -7,22 +7,85 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VCMyScanViewController: VCBaseViewController {
 
+    
+    
+    //容器
+    var customContainerView = UIView()
+    /// 冲击波视图
+    
+    var scanLineView =  UIImageView()
+    
+    ///框
+    
+    var borderIV = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "扫一扫"
         
-        
+        cameraView()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    func cameraView(){
+        // session
+        let mySession : AVCaptureSession = AVCaptureSession()
+        var myDevice : AVCaptureDevice? //= AVCaptureDevice()
+        let myImageOutput : AVCaptureStillImageOutput = AVCaptureStillImageOutput()
+        let devices = AVCaptureDevice.devices()
+        
+        let audioCaptureDevice = AVCaptureDevice.devices(withMediaType: AVMediaTypeAudio)
+        let audioInput = (try! AVCaptureDeviceInput(device: audioCaptureDevice?[0] as! AVCaptureDevice))  as AVCaptureInput
+        
+        for device in devices! {
+            if((device as AnyObject).position == AVCaptureDevicePosition.back){
+                myDevice = device as? AVCaptureDevice
+            }
+        }
+        
+        let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        print(status ==  AVAuthorizationStatus.authorized)
+        print(status ==  AVAuthorizationStatus.denied)
+        print(status ==  AVAuthorizationStatus.restricted)
+        print(status.rawValue)
+        
+        if status !=  AVAuthorizationStatus.authorized {
+            
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { (granted :Bool) -> Void in
+                if granted == false {
+                    // Camera not Authorized
+                    DispatchQueue.main.sync {
+                        print("Camera not Authorized")
+                        
+                        return
+                    }
+                }
+            });
+        }
     }
     
+    func checkCamera() {
+        
+        let authStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        switch authStatus {
+        case AVAuthorizationStatus.authorized:
+            print("AVAuthorizationStatus.Authorized")
+        case AVAuthorizationStatus.denied:
+            print("AVAuthorizationStatus.Denied")
+        case AVAuthorizationStatus.notDetermined:
+            print("AVAuthorizationStatus.NotDetermined")
+        case AVAuthorizationStatus.restricted:
+            print("AVAuthorizationStatus.Restricted")
+        default:
+            print("AVAuthorizationStatus.Default")
+        }
+        
+    
+    }
 
     /*
     // MARK: - Navigation
